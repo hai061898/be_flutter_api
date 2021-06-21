aa4575b59a05f35cd1abb507035e14e916c782c9
hai061898 <truonghaiauto98@gmail.com>
2021-06-21 15:49:34 +0700

    token

---
+++ app/Models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * Class User
+ * 
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ *
+ * @package App\Models
+ */
+class User extends Authenticatable implements JWTSubject
+{
+	use Notifiable;
+	protected $table = 'users';
+
+	protected $dates = [
+		'email_verified_at'
+	];
+
+	protected $hidden = [
+		'password',
+		'remember_token'
+	];
+
+	protected $fillable = [
+		'name',
+		'email',
+		'email_verified_at',
+		'password',
+		'remember_token'
+	];
+
+	public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+	public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
